3347a3e2c9799f07fb14a11e6d8d451ed9d57e25
VernL <vernon.lillies@gmail.com>
2018-04-02 00:53:38 -0400

    feat: fix pluggins and add prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,122 +1,53 @@
 module.exports = {
   siteMetadata: {
     title: `Vernon Lillies`,
-    description: 'A hybrid glob/portfolio',
-    siteUrl: 'www.vernonlillies.com'
+    description: "Vernon's blog and portfolio",
+    siteUrl: "https://www.vernonlillies.com"
   },
   plugins: [
-      {
-          resolve: `gatsby-transformer-remark`,
-          options: {
-              plugins: [
-                  {
-                      resolve: `gatsby-remark-prismjs`,
-                      options: {
-                          // Class prefix for <pre> tags containing syntax highlighting;
-                          // defaults to 'language-' (eg <pre class="language-js">).
-                          // If your site loads Prism into the browser at runtime,
-                          // (eg for use with libraries like react-live),
-                          // you may use this to prevent Prism from re-processing syntax.
-                          // This is an uncommon use-case though;
-                          // If you're unsure, it's best to use the default value.
-                          classPrefix: "language-",
-                      },
-                  },
-              ],
-          },
-      },
+    `gatsby-plugin-glamor`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`,
-      },
+        path: `${__dirname}/src/`
+      }
     },
-    `gatsby-plugin-glamor`,
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
+        pathToConfigModule: `src/utils/typography`
+      }
     },
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-feed`,
+      resolve: `gatsby-transformer-remark`,
       options: {
-        query: `
-        {
-          site {
-            siteMetadata {
-              title
-              description
-              siteUrl
-              site_url: siteUrl
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 1140
+            }
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              // Class prefix for <pre> tags containing syntax highlighting;
+              // defaults to 'language-' (eg <pre class="language-js">).
+              // If your site loads Prism into the browser at runtime,
+              // (eg for use with libraries like react-live),
+              // you may use this to prevent Prism from re-processing syntax.
+              // This is an uncommon use-case though;
+              // If you're unsure, it's best to use the default value.
+              classPrefix: "language-"
             }
           }
-        }
-      `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                });
-              });
-            },
-            query: `
-            {
-              allMarkdownRemark(
-                limit: 1000,
-                sort: { order: DESC, fields: [frontmatter___date] },
-                filter: {frontmatter: { draft: { ne: true } }}
-              ) {
-                edges {
-                  node {
-                    excerpt
-                    html
-                    fields { slug }
-                    frontmatter {
-                      title
-                      date
-                    }
-                  }
-                }
-              }
-            }
-          `,
-            output: "/rss.xml",
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-remark-images`,
-      options: {
-        // It's important to specify the maxWidth (in pixels) of
-        // the content container as this plugin uses this as the
-        // base for generating different widths of each image.
-        maxWidth: 960,
-        // Remove the default behavior of adding a link to each
-        // image.
-        linkImagesToOriginal: true,
-        // Analyze images' pixel density to make decisions about
-        // target image size. This is what GitHub is doing when
-        // embedding images in tickets. This is a useful setting
-        // for documentation pages with a lot of screenshots.
-        // It can have unintended side effects on high pixel
-        // density artworks.
-        //
-        // Example: A screenshot made on a retina screen with a
-        // resolution of 144 (e.g. Macbook) and a width of 100px,
-        // will be rendered at 50px.
-        //
-        // Defaults to false.
-        sizeByPixelDensity: false,
+        ]
       }
     },
     {
@@ -137,5 +68,5 @@ module.exports = {
         }
       }
     }
-  ],
+  ]
 };
